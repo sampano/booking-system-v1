bf4eedc060102b3590f431390e39374f24001bfe
sampano <samuelpano@gmail.com>
2025-09-17 20:40:22 +0800

    Updaate

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 /*import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> 3cd18e97ed7c499da9d05dbff9c7681b2229f807
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
@@ -18,9 +13,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/booking-system-v1/", // 👈 repo name for project site
+  base: "/booking-system-v1/",
 });
-=======
-  base: '/booking-system-v1/', // 👈 repo name for project site
-})
->>>>>>> 3cd18e97ed7c499da9d05dbff9c7681b2229f807
